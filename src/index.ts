9f028fcdee74eb521435e867e2704f71100607d5
Sameer Dewan <sameerdewan1994@gmail.com>
2023-06-10 13:19:08 -0400

    index ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import ContractService from './services/ContractService'
+import GroupService from './services/GroupService'
+import TokenService from './services/TokenService'
+import UriService from './services/UriService'
+
+export default {
+  ContractService,
+  GroupService,
+  TokenService,
+  UriService
+}
